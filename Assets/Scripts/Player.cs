6bb66905f1274d4dcbf5a07e36c1ced3ac99383d
Randy Powelson <randy.powelson@yahoo.com>
2024-05-16 23:05:17 -0700

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float movementSpeed;
+    private Rigidbody2D playerRb;
+    private float horizontal;
+
+
+    private void Awake(){
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(horizontal * movementSpeed * Time.deltaTime,0,0);
+    }
+
+    private void HandleJump(){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+
+}
